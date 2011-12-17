ef30af09d9d4524a01f89a9f2bb40f69f941e62c
Ingo Koerner <ingo.koerner@googlemail.com>
2011-12-17 15:01:38 +0100

    Main angepasst

--- src/main/java/de/datagenerator/Main.java
+++ src/main/java/de/datagenerator/Main.java
@@ -8,10 +8,10 @@ import de.datagenerator.generator.Context;
 public class Main {
 
 	public static void main(String[] args) {
-		long anzKunde = 5;
-		long anzProdukte = 5;
-		long anzWarenkoerbe = 3;
-		long anzProdukteInWarenkorb = 2;
+		long anzKunde = Long.valueOf(args[0]);
+		long anzProdukte = Long.valueOf(args[1]);
+		long anzWarenkoerbe = Long.valueOf(args[2]);
+		long anzProdukteInWarenkorb = Long.valueOf(args[3]);
 
 		Injector injector = Guice.createInjector(new DBModule());
 		Context context = injector.getInstance(Context.class);
