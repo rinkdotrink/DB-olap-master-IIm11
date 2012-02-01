eb57d9e987fcbb46b3f84bc7e041eb99a9340e62
Ingo Koerner <ingo.koerner@googlemail.com>
2012-02-01 19:53:47 +0100

    Beleg

--- ERD/explain partitions.sql
+++ ERD/explain partitions.sql
@@ -1,16 +1,8 @@
-EXPLAIN partitions
+EXPLAIN
 SELECT adbc.kunde.Name, SUM(adbc.produkt.Preis)
 FROM adbc.kunde,adbc.Produkt, adbc.Warenkorb, adbc.Warenkorb_has_Produkt
 WHERE produkt.PRODUKT_ID = warenkorb_has_produkt.Produkt_PRODUKT_ID 
     AND warenkorb_has_produkt.Warenkorb_WARENKORB_ID = warenkorb.WARENKORB_ID
-    AND warenkorb.Kunde_KUNDE_ID = kunde.KUNDE_ID
-    AND ((warenkorb_has_produkt.Datum = '2011-01-01')
-    OR   (warenkorb_has_produkt.Datum = '2011-01-05')
-    OR   (warenkorb_has_produkt.Datum = '2011-01-09')    
-    OR   (warenkorb_has_produkt.Datum = '2011-02-02')
-    OR   (warenkorb_has_produkt.Datum = '2011-02-06')    
-    OR   (warenkorb_has_produkt.Datum = '2011-02-10')
-    OR   (warenkorb_has_produkt.Datum = '2011-03-10')
-    OR   (warenkorb_has_produkt.Datum = '2011-03-14')    
-    OR   (warenkorb_has_produkt.Datum = '2011-03-18'))      
+    AND warenkorb.Kunde_KUNDE_ID = kunde.KUNDE_ID    
+    AND (warenkorb_has_produkt.Datum BETWEEN '2011-01-01' AND '2011-03-01')
 Group by adbc.kunde.Name;
